4ac394c3451a0a35e72f92ad63b8f3e71be36e66
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-02-21 00:59:32 +0530

    Expense Item component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import { ToastContainer } from "react-toastify";
-import PrivateRoutes from './routes/PrivateRoutes';
-import Signin from './components/Auth/Signin'
-import Signup from './components/Auth/Signup'
-import Modal from 'react-modal'
-import Home from './pages/Home'
-const { io } = require("socket.io-client");
+import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
 
-  const socket = io("http://localhost:4000");
-
-  socket.on("connect", () => {
-    console.log(socket.id);
-  });
-  
-  socket.on("disconnect", () => {
-    console.log('disconnected'); 
-  });
-
-  Modal.setAppElement("#root");
-
+ 
   return (
     <>
-    <ToastContainer />
-    <Routes>
-      <Route path="/" element={<Signin />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path="/" element={<PrivateRoutes />}>
-          <Route path="/home" element={<Home />} />
-      </Route>
-    </Routes>
+    <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </>
   );
 }
