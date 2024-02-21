c27fc3427e6d32b18de3fed2c2dce2ea941864e4
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-02-21 15:22:50 +0530

    Added Filter functionality

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -7,9 +7,13 @@ import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
-
+  const [filteredExpense,setFilteredExpense] = useState([props.items])
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    console.log(selectedYear)
+    const newList = props.items.filter((expense)=> selectedYear ===  expense.date.getFullYear())
+    console.log(newList)
+    setFilteredExpense(newList)
   };
 
   return (
@@ -19,7 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpense.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
